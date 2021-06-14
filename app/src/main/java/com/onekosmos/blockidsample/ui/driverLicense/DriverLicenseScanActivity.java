63ebd3b5ae3cccc2db8d9d462075f6531fc94578
Pankti1Kosmos <pankti@1kosmos.com>
2021-06-14 21:57:20 +0530

    ENGG2K-1731 - DL scan issue logs added.

--- app/src/main/java/com/onekosmos/blockidsample/ui/driverLicense/DriverLicenseScanActivity.java
+++ app/src/main/java/com/onekosmos/blockidsample/ui/driverLicense/DriverLicenseScanActivity.java
@@ -114,7 +114,7 @@ public class DriverLicenseScanActivity extends AppCompatActivity implements IDri
 
     private void registerDL(LinkedHashMap<String, Object> dlMap) {
         if(dlMap != null){
-            Log.d("Driver Barcode","===" + dlMap.get("barcodeInfo"));
+            Log.d("Driver City","===>" + dlMap.get("city"));
         }
         ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.show();
