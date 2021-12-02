ea4a05101800bdb9110b0eb92c05a2f42a1b4d61
Pankti1Kosmos <pankti@1kosmos.com>
2021-12-03 00:36:13 +0530

    ENGG2K-2455-Add-proxy-to-network-calls-via-SDK

--- app/src/main/java/com/onekosmos/blockidsample/MainApplication.java
+++ app/src/main/java/com/onekosmos/blockidsample/MainApplication.java
@@ -9,10 +9,16 @@ import com.onekosmos.blockid.sdk.BlockIDSDK;
  * Copyright © 2021 1Kosmos. All rights reserved.
  */
 public class MainApplication extends Application {
+    int proxyPort = 7580;
+    String proxyHost = "45.95.99.20";
+    final String username = "vautvdmg";
+    final String password = "ag2idbos8oo6";
+
     @Override
     public void onCreate() {
         super.onCreate();
         BlockIDSDK.initialize(this);
+        BlockIDSDK.getInstance().setProxy(proxyHost, proxyPort, username, password);
         BlockIDSDK.getInstance().setLicenseKey(AppConstant.licenseKey);
     }
 
