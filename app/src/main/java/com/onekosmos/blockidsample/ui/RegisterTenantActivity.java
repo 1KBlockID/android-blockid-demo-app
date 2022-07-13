f52499d747a7302d3b1e7a120b6814d9a2319e97
Sarthak Mishra <sarthak.mishra@1kosmos.com>
2022-07-13 15:09:42 +0530

    MOB-3174-android-consume-updated-sdk-in-block-id-applications

--- app/src/main/java/com/onekosmos/blockidsample/ui/RegisterTenantActivity.java
+++ app/src/main/java/com/onekosmos/blockidsample/ui/RegisterTenantActivity.java
@@ -81,7 +81,7 @@ public class RegisterTenantActivity extends AppCompatActivity {
                 errorDialog.showNoInternetDialog(onDismissListener);
                 return;
             }
-            errorDialog.showWithOneButton(null, getString(R.string.label_error), error.getMessage(), onDismissListener);
+            errorDialog.showWithOneButton(null, getString(R.string.label_error), error.getMessage(), getString(R.string.label_ok), onDismissListener);
         });
     }
 
