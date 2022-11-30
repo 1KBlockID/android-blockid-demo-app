9f590b1e675805212d0298c3b7007a119d49cdac
Anooj <sarthak.mishra@1kosmos.com>
2022-11-30 17:48:43 +0530

    MOB-3662-implement-drivers-license-document-scanning

---
+++ app/src/main/java/com/onekosmos/blockidsample/ui/documentScanner/DocumentScannerActivity.java
@@ -0,0 +1,25 @@
+package com.onekosmos.blockidsample.ui.documentScanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.onekosmos.blockidsample.R;
+
+/**
+ * Created by 1Kosmos Engineering
+ * Copyright © 2022 1Kosmos. All rights reserved.
+ */
+public class DocumentScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_document_scanner);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+}
