97045b0b88812b7271c8bfb6825b6add7dc9e0f1
Sarthak Mishra <sarthak.mishra@1kosmos.com>
2022-12-02 20:11:02 +0530

    composecure

--- app/src/main/java/com/onekosmos/blockidsample/AppConstant.java
+++ app/src/main/java/com/onekosmos/blockidsample/AppConstant.java
@@ -7,9 +7,9 @@ import com.onekosmos.blockid.sdk.datamodel.BIDTenant;
  * Copyright © 2021 1Kosmos. All rights reserved.
  */
 public class AppConstant {
-    public static final String licenseKey = "5809b7b7-886f-4c88-9061-59a2baf485be";
-    public static final String dvcId = "default_config";
+    public static final String licenseKey = "d2be2bd0-91e7-4374-9594-843f770bfa6c";
+    public static final String dvcId = "composecure_09e2a012-d0ac-4695-a909-0b4d54198c58";
     public static final BIDTenant defaultTenant = new BIDTenant("1kosmos", "default",
-            "https://1k-dev.1kosmos.net");
+            "https://1k-pilot.1kosmos.net");
     public static final String documentScannerLicense = "AwEpN8bqpYsM38c19HW2T6/W8OuW+DxiMHlowro13y0o3Td68zqWA4bkl15omB5c9AsDpB32QPm79nQoyrpPfU+TBpHiwu9hL0Lp9J/vfGpaUz2GqEnBTcQs6uv4Xa84+iRMOyJ/nrExwwJBl9gRugVt";
 }
