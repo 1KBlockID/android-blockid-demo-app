c030151ed2d75e8486d961b1847ebe021d759e4d
Pankti1Kosmos <pankti@1kosmos.com>
2023-01-03 18:47:57 +0530

    MOB-3756- code clean up

--- app/src/main/java/com/onekosmos/blockidsample/ui/driverLicense/DriverLicenseScanActivity.java
+++ app/src/main/java/com/onekosmos/blockidsample/ui/driverLicense/DriverLicenseScanActivity.java
@@ -1,10 +1,5 @@
 package com.onekosmos.blockidsample.ui.driverLicense;
 
-import static com.onekosmos.blockid.sdk.BIDAPIs.APIManager.ErrorManager.CustomErrors.K_SOMETHING_WENT_WRONG;
-import static com.onekosmos.blockid.sdk.cameramodule.dlScanner.DLScanningOrder.FIRST_BACK_THEN_FRONT;
-import static com.onekosmos.blockid.sdk.document.BIDDocumentProvider.RegisterDocCategory.identity_document;
-import static com.onekosmos.blockid.sdk.document.RegisterDocType.DL;
-
 import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -39,6 +34,11 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.LinkedHashMap;
+
+import static com.onekosmos.blockid.sdk.BIDAPIs.APIManager.ErrorManager.CustomErrors.K_SOMETHING_WENT_WRONG;
+import static com.onekosmos.blockid.sdk.cameramodule.dlScanner.DLScanningOrder.FIRST_BACK_THEN_FRONT;
+import static com.onekosmos.blockid.sdk.document.BIDDocumentProvider.RegisterDocCategory.identity_document;
+import static com.onekosmos.blockid.sdk.document.RegisterDocType.DL;
 
 
 /**
@@ -261,7 +261,9 @@ public class DriverLicenseScanActivity extends AppCompatActivity implements View
 //            mLayoutMessage.setVisibility(View.VISIBLE);
 //            mTxtMessage.setVisibility(View.VISIBLE);
 //            mTxtMessage.setText(R.string.label_scanning);
-            mDriverLicenseScannerHelper = new DLScannerHelper(this, this);
+            mDriverLicenseScannerHelper = new DLScannerHelper(this,
+                    FIRST_BACK_THEN_FRONT,
+                    this);
             mDriverLicenseScannerHelper.startScanning();
         }
     }
