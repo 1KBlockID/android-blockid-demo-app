166d88ea8eb4f765e4efcbbb4f93b4fd745f3548
Pankti1Kosmos <pankti@1kosmos.com>
2023-02-01 00:46:20 +0530

    MOB-3842-mobile-sdk-getting-api-not-allowed-because-its-calling-wrong-dns-for-managed-wallet- in progress

--- app/src/main/java/com/onekosmos/blockidsample/AppConstant.java
+++ app/src/main/java/com/onekosmos/blockidsample/AppConstant.java
@@ -10,5 +10,12 @@ public class AppConstant {
     public static final String licenseKey = "5809b7b7-886f-4c88-9061-59a2baf485be";
     public static final String dvcId = "default_config";
     public static final BIDTenant defaultTenant = new BIDTenant("1kosmos", "default",
-            "https://1k-dev.1kosmos.net");
+            "https://1k-qa.1kosmos.net");
+    public static final BIDTenant clientTenant = new BIDTenant("acme", "default",
+            "https://blockid-qa.1kosmos.net");
+
+//    WHERE IS MY ROOT
+//    open my SD (always at {dns}/caas/sd)
+//    open global_caas SD › {my SD}.global_caas + “/sd”
+//    Root = “{my SD}.global_caas” + “/sd” . adminconsole
 }
