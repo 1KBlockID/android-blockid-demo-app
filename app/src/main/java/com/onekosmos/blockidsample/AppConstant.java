ec382ff30bac79a939123c083d4011a581688b7c
Pankti1Kosmos <pankti@1kosmos.com>
2023-02-01 18:45:44 +0530

    MOB-3842-Updated tenant details

--- app/src/main/java/com/onekosmos/blockidsample/AppConstant.java
+++ app/src/main/java/com/onekosmos/blockidsample/AppConstant.java
@@ -9,8 +9,10 @@ import com.onekosmos.blockid.sdk.datamodel.BIDTenant;
 public class AppConstant {
     public static final String licenseKey = "5809b7b7-886f-4c88-9061-59a2baf485be";
     public static final String dvcId = "default_config";
-    public static final BIDTenant defaultTenant = new BIDTenant("1kosmos", "default",
-            "https://1k-qa.1kosmos.net");
-    public static final BIDTenant clientTenant = new BIDTenant("acme", "default",
-            "https://blockid-qa.1kosmos.net");
+    public static final BIDTenant defaultTenant = new BIDTenant("1kosmos",
+            "default",
+            "https://1k-dev.1kosmos.net");
+    public static final BIDTenant clientTenant = new BIDTenant("blockiddev-1kosmos",
+            "default",
+            "https://blockid-dev.1kosmos.net");
 }
