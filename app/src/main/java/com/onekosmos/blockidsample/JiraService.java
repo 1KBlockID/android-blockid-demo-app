bbb697aa6ca28183b8c09a52a88c337b72822e66
gaurav.rane <gaurav.rane@1kosmos.com>
2024-10-30 12:03:43 +0530

    MOB-6237-report-a-problem-from-mobile-app-to-jira-board

--- app/src/main/java/com/onekosmos/blockidsample/JiraService.java
+++ app/src/main/java/com/onekosmos/blockidsample/JiraService.java
@@ -4,7 +4,8 @@ import android.util.Log;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.error.ANError;
-import com.androidnetworking.interfaces.ParsedRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.androidnetworking.interfaces.OkHttpResponseListener;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -12,51 +13,75 @@ import org.json.JSONObject;
 import java.io.IOException;
 
 import okhttp3.Credentials;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
 
 public class JiraService {
 
     private static final String JIRA_URL = "https://onekosmos.atlassian.net/rest/api/2/issue";
+
+    private static final String JIRA_URL_GET = "https://onekosmos.atlassian.net/rest/api/2/project/10052";
     private static final String JIRA_EMAIL = "gaurav.rane@1kosmos.com";
-    private static final String JIRA_API_TOKEN = "ATATT3xFfGF0Ou6oo7fAKurEXbFCtjoCKpFuJOlEWiUSCH8azZ7m3FesMm72yWxVcs14a006mFUgY4Go_MLgaUvYrExxALoFNgfijI6z-8pPwzoZiQ6FfcVyYE74YS2lfGPLEPNv3ymYFI_-85C-zYexLjG9dVV36DwwjFDYlSuH3Zg4pVXAAj8=305A1735"; // Or password for Jira Server
+    private static final String JIRA_API_TOKEN = "ATATT3xFfGF0bF1cs4MV2DaDg5XfVDt4cFoI_xDGusNHqxzTxB6zrdD50jdZ2nf-klGnH1a6J9OHLHfXMAzN_Mcg7DP_2FoFgMHW-nDQ9is1mJjx6R7zKA3GVmIVjor3nckRqJEMlBruxAOSha3i8R3JCTagiuSpJut8L6vr_zVXf0iLGzUtUbk=E650DAC1"; // Or password for Jira Server
 
     public void createJiraTicket() throws IOException {
         // Create the JSON body for the request
         JSONObject issueData = new JSONObject();
-        JSONArray components = new JSONArray();
-
         try {
-            JSONObject android = new JSONObject();
-            android.put("id", "10123");
-            components.put(android);
             issueData.put("fields", new JSONObject()
-                    .put("project", new JSONObject().put("id", "10016"))  // Replace PROJECT_KEY with your Jira project key
-                    .put("components", components)
                     .put("summary", "This is a test issue created from Android")
-                    .put("description", "Test Detailed description of the issue")
-                    .put("issuetype", new JSONObject().put("name", "Bug")));    // Replace 'Bug' with your issue type
+                    .put("issuetype", new JSONObject().put("name", "Bug"))  // Replace 'Bug' with your issue type
+                    .put("project", new JSONObject().put("key", "FEED"))  // Replace PROJECT_KEY with your Jira project key
+                    //.put("components", components)
+                    .put("description", "Test Detailed description of the issue."));
         } catch (Exception e) {
             e.printStackTrace();
         }
         Log.d("JiraPayload", issueData.toString());
+
         // Add basic authentication header (Base64 encoded)
+        //getJIRAIssue();
         String credential = Credentials.basic(JIRA_EMAIL, JIRA_API_TOKEN);
         AndroidNetworking.post(JIRA_URL)
                 .addHeaders("Authorization", credential)
+                .addHeaders("Accept", "*/*")
                 .addHeaders("Content-Type", "application/json")
-                .addApplicationJsonBody(issueData)
+                .addJSONObjectBody(issueData)
                 .doNotCacheResponse()
                 .build()
-                .getAsObject(JSONObject.class, new ParsedRequestListener<JSONObject>() {
+                .getAsJSONObject(new JSONObjectRequestListener() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d("jira ticket response:", response.toString());
-
+                        Log.e("jira ticket success for POST API:", "success:" + response.toString());
                     }
 
                     @Override
                     public void onError(ANError anError) {
-                        Log.e("jira ticket failure:", anError.getErrorBody());
+                        Log.e("jira ticket failure for POST API:", "Error:" + anError.toString());
                     }
                 });
     }
+
+    public void getJIRAIssue() {
+        String credential = Credentials.basic(JIRA_EMAIL, JIRA_API_TOKEN);
+        AndroidNetworking.get(JIRA_URL_GET)
+                .addHeaders("Authorization", credential)
+                .addHeaders("Accept", "*/*")
+                .addHeaders("Content-Type", "application/json")
+                .doNotCacheResponse()
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("jira ticket success for GET API:", "success:" + response.toString());
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.e("jira ticket failure for GET API:", "Error:" + anError.toString());
+                    }
+                });
+    }
+
+
 }
