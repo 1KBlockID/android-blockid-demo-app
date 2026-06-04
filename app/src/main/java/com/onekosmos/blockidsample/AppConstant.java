e5f694239482cf194e303fefc6df56147f2f8e2b
Vaishali1K <vaishali@1kosmos.com>
2026-06-04 13:54:34 +0530

    fixed co-pilot comments

--- app/src/main/java/com/onekosmos/blockidsample/AppConstant.java
+++ app/src/main/java/com/onekosmos/blockidsample/AppConstant.java
@@ -8,8 +8,8 @@ import com.onekosmos.blockid.sdk.datamodel.BIDTenant;
  */
 public class AppConstant {
     public static final String licenseKey = "5809b7b7-886f-4c88-9061-59a2baf485be";
-    public static final String dvcId = "default_dvcid";
-    public static final BIDTenant defaultTenant = new BIDTenant("omega-client",
+    public static final String dvcId = "default_config";
+    public static final BIDTenant defaultTenant = new BIDTenant("blockid-dev",
             "default",
-            "https://omega-client.1kosmos.net");
+            "https://blockid-dev.1kosmos.net");
 }
